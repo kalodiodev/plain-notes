d4151545a51b0e5fe330c18c4ff72ec7977f3d62
Athanasios Raptodimos <thanosra@gmail.com>
2018-05-21 20:24:08 +0300

    Add database name

--- configuration.php
+++ configuration.php
@@ -6,7 +6,7 @@
 return [
     'url_prefix' => '',
 
-    'database_name' => '',
+    'database_name' => 'plain-notes',
     'database_username' => 'root',
     'database_password' => 'secret',
     'database_host' => 'localhost'
