df93ead3caf317a2bffbe25eaf7b057a06c0332a
Athanasios Raptodimos <thanosra@gmail.com>
2018-05-21 20:23:01 +0300

    Refactor to use message helper

--- app/views/partial/page_error.php
+++ app/views/partial/page_error.php
@@ -1,8 +1,5 @@
 <?php if(isset($_SESSION['error_message'])): ?>
     <div class="alert alert-danger">
-        <?php
-        echo $_SESSION['error_message'];
-        unset($_SESSION['error_message']);
-        ?>
+        <?php echo get_error_message(); ?>
     </div>
 <?php endif; ?>
