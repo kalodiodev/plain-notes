1c33b5c7d2743892651ea86d71d8a81056fce9f5
Athanasios Raptodimos <thanosra@gmail.com>
2018-05-28 22:21:18 +0300

    Add form errors session setter helper

--- core/helpers/form-error.php
+++ core/helpers/form-error.php
@@ -1,6 +1,30 @@
 <?php
 
+/**
+ * Check if form field has post/validation errors
+ *
+ * @param $field
+ * @return bool
+ */
 function field_has_error($field)
 {
     return (! empty($_SESSION['errors'][$field]) && (sizeof($_SESSION['errors'][$field])) > 0);
+}
+
+/**
+ * Set form post/validation errors
+ *
+ * @param $errors
+ */
+function set_form_errors($errors)
+{
+    $_SESSION['errors'] = $errors;
+}
+
+/**
+ * Clear all form errors from Session
+ */
+function clear_form_errors()
+{
+    unset($_SESSION['errors']);
 }
