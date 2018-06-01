169567120eb4d8a3b39e5f41a1b24fba02be3503
Athanasios Raptodimos <thanosra@gmail.com>
2018-06-01 17:33:34 +0300

    Add serialization

--- app/models/Note.php
+++ app/models/Note.php
@@ -3,13 +3,14 @@
 namespace App\Model;
 
 use App\Core\Model;
+use Serializable;
 
 /**
  * Class Note
  * 
  * @package App\Model
  */
-class Note extends Model {
+class Note extends Model implements Serializable {
 
     public $id;
     public $title;
@@ -22,4 +23,35 @@ class Note extends Model {
     public $validate_fields = [];
 
 
+    /**
+     * String representation of Note object
+     *
+     * @return string the string representation of the Note object or null
+     */
+    public function serialize()
+    {
+        return serialize([
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'details' => $this->details,
+            'exp_date' => $this->exp_date
+        ]);
+    }
+
+    /**
+     * Constructs the Note object from string representation
+     *
+     * @param string $serialized string representation of Note object
+     */
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+
+        $this->id = $data['id'];
+        $this->title = $data['title'];
+        $this->description = $data['description'];
+        $this->details = $data['details'];
+        $this->exp_date = $data['exp_date'];
+    }
 }
