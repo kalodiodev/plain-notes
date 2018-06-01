cd66bd3a3c9392dea5554fea5858114eef361e89
Athanasios Raptodimos <thanosra@gmail.com>
2018-06-01 17:34:26 +0300

    Fix sources url

--- app/views/partial/head.php
+++ app/views/partial/head.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-COMPATIBLE" content="ie=edge">
     <title><?php echo isset($title) ? $title : "Plain Notes"; ?></title>
 
-    <link rel="stylesheet" href="public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/css/styles.css">
-    <script src="public/js/bootstrap.min.js"></script>
-    <script src="public/js/jquery-3.3.1.slim.min.js"></script>
-    <script src="public/js/popper.min.js"></script>
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/styles.css">
+    <script src="../public/js/bootstrap.min.js"></script>
+    <script src="../public/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../public/js/popper.min.js"></script>
 </head>
