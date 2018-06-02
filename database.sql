dcf50bf2c4923d469ef4320a8da575588c42bd27
Athanasios Raptodimos <thanosra@gmail.com>
2018-06-02 21:28:45 +0300

    Add create database sql

---
+++ database.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA `plain-notes`;
+
+CREATE TABLE `plain-notes`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `plain-notes`.`notes` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(65) NOT NULL,
+  `description` VARCHAR(255) DEFAULT NULL,
+  `exp_date` VARCHAR(45) DEFAULT NULL,
+  `user_id` INT(11) NOT NULL,
+  `created_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `details` TEXT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `plain-notes`.`users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB CHARSET=utf8;
