f06f926c49978a27208b1f4b1a3019e20727cf08
Athanasios Raptodimos <thanosra@gmail.com>
2018-06-10 18:05:13 +0300

    Add user menu at navbar

--- app/views/partial/head.php
+++ app/views/partial/head.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="../public/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/styles.css">
-    <script src="../public/js/bootstrap.min.js"></script>
-    <script src="../public/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../public/js/jquery-3.3.1.min.js"></script>
     <script src="../public/js/popper.min.js"></script>
+    <script src="../public/js/bootstrap.min.js"></script>
 </head>
--- app/views/partial/navbar.php
+++ app/views/partial/navbar.php
@@ -9,23 +9,43 @@
             <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('') ? 'active' : ''; ?>">
                 <a class="nav-link" href="<?php echo route(''); ?>">Home <span class="sr-only">(current)</span></a>
             </li>
-            <?php if(! isAuthenticated()): ?>
-            <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('login') ? 'active' : ''; ?>">
-                <a class="nav-link" href="<?php echo route('login'); ?>">Login</a>
-            </li>
-            <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('register') ? 'active' : ''; ?>">
-                <a class="nav-link" href="<?php echo route('register'); ?>">Register</a>
-            </li>
-            <?php endif; ?>
 
             <?php if(isAuthenticated()): ?>
-            <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('notes') ? 'active' : ''; ?>">
-                <a class="nav-link" href="<?php echo route('notes'); ?>">Notes</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?php echo route('logout'); ?>">Logout</a>
-            </li>
+                <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('notes') ? 'active' : ''; ?>">
+                    <a class="nav-link" href="<?php echo route('notes'); ?>">Notes</a>
+                </li>
             <?php endif; ?>
         </ul>
+
+        <?php if(! isAuthenticated()): ?>
+            <ul class="navbar-nav">
+                <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('login') ? 'active' : ''; ?>">
+                    <a class="nav-link" href="<?php echo route('login'); ?>">Login</a>
+                </li>
+                <li class="nav-item <?php echo $_SERVER['REQUEST_URI'] === route('register') ? 'active' : ''; ?>">
+                    <a class="nav-link" href="<?php echo route('register'); ?>">Register</a>
+                </li>
+            </ul>
+        <?php endif; ?>
+
+        <?php if(isAuthenticated()): ?>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php
+                            if(strlen(auth()->firstName) > 0 && strlen(auth()->lastName) > 0) {
+                                echo auth()->firstName . ' ' . auth()->lastName;
+                            } else {
+                                echo auth()->email;
+                            }
+                        ?>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="<?php echo route('logout'); ?>">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        <?php endif; ?>
     </div>
 </nav>
