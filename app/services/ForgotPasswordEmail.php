c1a022f3696046c20c0484e0249be7ea287406e2
Athanasios Raptodimos <thanosra@gmail.com>
2018-06-27 20:10:43 +0300

    Add forgot password email

---
+++ app/services/ForgotPasswordEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Confirm Email
+ *
+ * @package App\Services
+ */
+class ForgotPasswordEmail extends Email
+{
+    protected $subject = "Your password reset link...";
+
+    /**
+     * Prepare email to send
+     *
+     * @param $data
+     * @return string
+     */
+    protected function prepare($data)
+    {
+        global $config;
+
+        $href = $config['reset_password'] . '?token=' . $data['token'];
+
+        $message = <<<EOD
+        <!doctype html>
+        <html>
+        <body>
+        <h1>Reset your password</h1>
+        <p>To reset your password click <a href="$href">Here</a></p>
+        <p>This link expires in 24 hours</p>
+        </body>
+        </html>
+EOD;
+
+        return $message;
+    }
+}
